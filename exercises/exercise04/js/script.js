fe06e8507115a6b09cf17e8b9627f87956126815
SevanBelleau <sevanbelleau@gmail.com>
2018-02-06 16:37:46 -0500

    Finishing touches

--- exercises/exercise04/js/script.js
+++ exercises/exercise04/js/script.js
@@ -12,6 +12,19 @@ $(document).ready(function () {
 
   // Insert jQuery code here to run when the page is loaded
 
+  // if ($(window).css("width") >= 600 {
+  //   $('body').css({backgroundColor:'#f00'});
+  // }
+
+setInterval(function() {
+  if ($(window).width() > 600) {
+    $("#nope").hide();
+  }
+  else {
+    $("#nope").show();
+  }
+  
+},100);
 
 
 });
