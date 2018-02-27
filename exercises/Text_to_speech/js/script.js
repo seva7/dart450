b4d548469f1e5df60da725e133337b3cc2274d01
SevanBelleau <sevanbelleau@gmail.com>
2018-02-27 15:55:54 -0500

    tests

---
+++ exercises/Text_to_speech/js/script.js
@@ -0,0 +1,18 @@
+/**********************************************
+
+DART 450, Winter 2018
+Title of Project
+Author
+
+Description of what the script does...
+
+**********************************************/
+
+$(document).ready(function () {
+
+  // Insert jQuery code here to run when the page is loaded
+responsiveVoice.speak("Papa", "UK English Male", {volume: .1});
+responsiveVoice.speak("Papa", "UK English Male", {volume: .2});
+responsiveVoice.speak("Papa", "UK English Male", {volume: .4});
+responsiveVoice.speak("bless", "UK English Male", {volume: 1});
+});
