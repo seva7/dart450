d723235140320c478c7622387edab107f845d9e2
SevanBelleau <sevanbelleau@gmail.com>
2018-03-06 17:55:24 -0500

    trying to input manifesto

---
+++ exercises/tests/js/script.js
@@ -0,0 +1,17 @@
+/**********************************************
+
+DART 450, Winter 2018
+Title of Project
+Author
+
+Description of what the script does...
+
+**********************************************/
+
+$(document).ready(function () {
+
+  // Insert jQuery code here to run when the page is loaded
+
+text.string('hello')
+
+});
--- wearenothere/js/script.js
+++ wearenothere/js/script.js
@@ -8,7 +8,24 @@ Description of what the script does...
 
 **********************************************/
 
+
+// var manifestoText: ['A designer is the übermensch, the liberator the creator the comedian the chef the negotiator the deal breaker the fanatic the fuck you and your ideals the loudest voice the always got something to say the call you out on your bullshit the monkey the self revealing self destructive constructive the respectful disrespect the historian the political the unafraid the always wanting better the helpful hand the fuck you.',
+// 'Because we’ve had enough of all this. The values that have become apparent are not representative. We are being made to follow orthodox ideals that try to render any and all opposition useless. These ideals are harmful to the individuals mental state within the society. What we, and this manifesto puts forth, is to allow us, without contest and with cooperation, to become ourselves.','We write these words as people under 30, currently being welcomed to the human race. We won’t be complicit. As if anyone would care and they don’t, but it doesn’t matter because it is not about them it is about us, of our knowing of it and acting on it. We must realize what was lost and what was given, how much of a privilege it is and maximize and reciprocate it for others. ','We must constantly ask ourselves; “Is what was destroyed for me worth what I will make with it?” We have a duty to continue, as we are the future that the past dreamed of.',
+// 'You have a duty if you call yourself a designer, a creator, an artist, a member of society. Your beliefs; everything you wish for, everything you want, everything you have seen, everything you adhere to is in your work.',
+// '']
+// var completeManifesto: ['']
 $(document).ready(function () {
+
+
+
+
+// You have a duty if you call yourself a designer, a creator, an artist, a member of society. Your beliefs; everything you wish for, everything you want, everything you have seen, everything you adhere to is in your work.
+
+
+
+
+
+
 
 
   $('img').each(function () {
@@ -52,6 +69,22 @@ $(document).ready(function () {
   });
 
 
+// this code makes the images move by themselves without mouse over!
+  // setInterval(function () {
+  //   $('img').each(function () {
+  //   var a = Math.random() * 500;
+  //   var b = Math.random() * 800;
+  //   var duration =
+  //   // Math.random() *
+  //   3000;
+  //   $(this).animate({
+  //     top: a,
+  //     left: b,
+  //   },duration);
+  // });
+  // },3000);
+
+
   $('#submit').on('mouseover',function () {
     var a = Math.random() * 500;
     var b = Math.random() * 800;
@@ -66,6 +99,7 @@ $(document).ready(function () {
 
 
   $('#email').on('mouseover',function () {
+    console.log("Moused over email.")
     var a = Math.random() * 500;
     var b = Math.random() * 800;
     var duration =
