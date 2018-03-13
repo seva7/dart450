325adf1f292efe813151458c109db09dd97fae51
SevanBelleau <sevanbelleau@gmail.com>
2018-03-13 15:39:29 -0400

    Code reset

--- exercises/exercise07/js/script.js
+++ exercises/exercise07/js/script.js
@@ -8,27 +8,5 @@ Pippin Barr
 
 $(document).ready(function() {
 
-  $('#testText').blast({
-      delimiter: 'word'
-    }).mouseover(makeEasy);
 
-    // //
-    // function fadeToPink () {
-    //   $(this).animate({
-    //       color: '#DA70D6	',
-    //   },800);
-    //   $(this).css({
-    //     'font-family':'comic sans MS'
-    //   });
-    //   $(this).animate({
-    //     color: 'blue',
-    //   }, 600);
-
-  });
-
-    function makeEasy() {
-      $(this).attr({
-        class: "happy"
-        
-      })
     };
