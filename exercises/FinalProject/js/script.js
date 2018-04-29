e89efe2c41d8ef4cb6de7d1dc33a5508d537c982
SevanBelleau <sevanbelleau@gmail.com>
2018-04-29 12:29:25 -0400

    I keep trying to figure it OUT BUT ITS NOT FRAKING WORKING. Sad sad sad :'(

--- exercises/FinalProject/js/script.js
+++ exercises/FinalProject/js/script.js
@@ -54,12 +54,29 @@ submitButton.addEventListener("click", onInput);
 // This is sentence is adding a listener for any submit cues (which would come for the form) to trigger the handleForm function.
 form.addEventListener("submit", handleForm);
 
-
+// All this code is before the document.ready because it is an anonymous function that calls upon itself as the page is loaded.
 })();
 
 $(document).ready(function () {
-  // All our code goes in here!
 
+var otherPosts = ["pics/audreypost.png","pics/vice.png","pics/charles.png","pics/mass.png"];
+var numPosts = 4;
+var posts =Math.floor((Math.random()*10)) % otherPosts.length
+  var testDiv = $('<img class="drawnPosts" id="drawnPosts">');
+  $('.make').append(testDiv);
+  $('#drawnPosts').attr("src", otherPosts[posts]);
+
+
+
+
+var time = Math.random() * 30000;
+console.log(time)
+
+// I wanted to make a nicer way but I didnt manage to.
+
+// Here is the first test of my second big part of code. I wanted a random post to appear every now and then to give some life to the BrutalBook website.
+// The idea is that every x amount of time, jquery would append a new img in my div with the posts and would do so by creating an open div that would
+// then have an attr associated to it that defined its source. This would mean I don't need same code multiple time but for some reason I can't make it work.
 // var interval = an amount of time but I want that to change every x amount of time
 // var otherPosts = ["pics/audreypost.png","pics/blank.png","pics/charles.png","pics/mass.png"];
 // var numPosts = 4;
@@ -68,36 +85,24 @@ $(document).ready(function () {
 // $('.make').append(testDiv);
 // $('#drawnPosts').attr("src", otherPosts[i]);
 
+// Here I am trying to see if i++ can make the posts post more than once, seeing that is the most I can do for now.
+// var otherPosts = ["pics/audreypost.png","pics/blank.png","pics/charles.png","pics/mass.png"];
+// var posts =Math.floor((Math.random()*10)) % otherPosts.length;
+// var $newPostPics = $('<img class="drawnPosts" id="drawnPosts">');
+// var i = 0
+// var appendDiv = setInterval(function(){
+//   if (i===4) {
+//     clearInterval(appendDiv);
+//   }
+//   else {
+//     console.log(i);
+//   $(".make").append($newPostPics)
+//   $("#drawnPosts").attr("src", otherPosts[posts]);
+// }
+// i++
+// },5000)
 
-
-
-// setInterval(function(){
-// var time = Math.random() * 30000;
-// console.log(time);
-// },60000);
-
-var otherPosts = ["pics/audreypost.png","pics/blank.png","pics/charles.png","pics/mass.png"];
-var i =Math.floor((Math.random()*10)) % otherPosts.length;
-var $newPostPics = $('<img class="drawnPosts" id="drawnPosts">');
-
-
-var time = Math.random() * 1000;
-console.log(time);
-setInterval(function(){
-$('.make').append($newPostPics);
-$(this).attr("src", otherPosts[i]);
-var timesAppended = 0;
-timesAppended = timesAppended + 1;
-console.log(timesAppended)
-},time);
-
-
-
-
-
-
-// console.log(amountOfTime)
-
+// Here I was trying to count the time the posts were appended. The var interval would have been changed with my random time one.
 // var $newPostPics = $('<img src="pics/frame.png" class="drawnPosts">')
 // var interval = 1000;
 // setInterval(function() {
@@ -107,6 +112,11 @@ console.log(timesAppended)
 // // console.log(timesAppended)
 // },interval);
 
+//Here I was trying to make a value of time that refreshed, so it felt more human and less here is a post every 5 seconds
+// setInterval(function(getTime){
+// var time = Math.random() * 30000;
+// // console.log(time);
+// },5000);
 
 
 
